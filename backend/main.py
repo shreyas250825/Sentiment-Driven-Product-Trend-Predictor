5f50afdef06dd69926849d2617ff69dcf6dab028
SimuHireAI <shreyassalian0425@gmail.com>
2025-09-09 21:07:47 +0530

    Vercel-4

--- backend/main.py
+++ backend/main.py
@@ -77,7 +77,12 @@ app = FastAPI(
 # --- CORS ---
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://sentiment-driven-product-trend-predictor-gw8eb1lb3.vercel.app",
+        "https://sentiment-driven-product-trend-predictor.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
